41bd42e6b0e09c8ee6a4cba8be034ec34ad82b70
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-01-20 19:28:53 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  User.create(email: Faker::Internet.email, password: "123456", username: Faker::Internet.username(specifier: 5..8))
+end
+
+puts 'Users created.'
