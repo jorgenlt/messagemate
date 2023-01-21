5cd369025080b4e80eff810132ff13b39b1d8ca1
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-01-21 19:20:25 +0800

    save

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,17 +6,18 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-    raise
+    @message.user_id = current_user.id
     # @message.recipient_id = params[:recipient_id]
     if @message.save
       flash.notice = "Message sent"
+      redirect_to user_messages_path(current_user.id)
     else
       flash.alert = "Error. Message was not sent."
     end
   end
 
   def message_params
-    raise
+
     params.require(:message).permit(:message_body, :recipient_id)
   end
 end
