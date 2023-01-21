7733f9838fede39761029b8df73160d4ca72ec24
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-01-21 15:41:23 +0800

    controllers and views

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,17 @@
 class ChatsController < ApplicationController
 
   def chats
-    @chats = 'something'
+    @messages = Message.all
+  end
+
+  def create
+    @message = Message.new(message_params)
+  end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:message_body)
   end
 end
