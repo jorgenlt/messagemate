c2f8a61f2efb0ae4423c396911b6bd6bbe462f74
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-01-24 15:21:13 +0800

    save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
+
+  # redirects to sign in page when user logs out
+  def after_sign_out_path_for(user)
+    user_session_path
+  end
 end
