a8e6d7a0f4fefd3b8913b036946a071be3142684
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-01-24 21:55:40 +0800

    desktop app complete.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,33 +2,37 @@ class MessagesController < ApplicationController
   # skip authorization on MessagesController with the before_action
   before_action :skip_authorization
 
-
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
     @message = Message.new(message_params)
     @message.chatroom_id = @chatroom.id
     @message.user_id = current_user.id
 
-    # setting sender/receiver of the message
-    if current_user.id == @chatroom.user_id
-      @message.recipient_id = @chatroom.recipient_id
+    if message_params[:message_body].empty?
+      # submit button 'send' does nothing.
     else
-      @message.recipient_id = @chatroom.user_id
-    end
+      # setting sender/receiver of the message
+      if current_user.id == @chatroom.user_id
+        @message.recipient_id = @chatroom.recipient_id
+      else
+        @message.recipient_id = @chatroom.user_id
+      end
 
-    # instant message with action cable
-    if @message.save
-      ChatroomChannel.broadcast_to(
-        @chatroom,
-        message: render_to_string(partial: "message", locals: { message: @message }),
-        sender_id: @message.user.id
-      )
-      head :ok
-    else
-      render "chatrooms/show", status: :unprocessable_entity
-      flash.alert = "Error. Message was not sent."
+      # instant message with action cable
+      if @message.save
+        ChatroomChannel.broadcast_to(
+          @chatroom,
+          message: render_to_string(partial: "message", locals: { message: @message }),
+          sender_id: @message.user.id
+        )
+        head :ok
+      else
+        render "chatrooms/show", status: :unprocessable_entity
+        flash.alert = "Error. Message was not sent."
+      end
     end
   end
+
 
   def message_params
     params.require(:message).permit(:message_body, :chatroom_id)
